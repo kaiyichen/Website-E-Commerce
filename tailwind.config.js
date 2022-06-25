e243ba6fd12c401bb899569bdae2f4e995b9bd03
kaiyichen <chenkaiyi01@gmail.com>
2022-06-26 00:29:25 +0800

    fin-v1

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [style.css, index.html, tailwind.css. index.js],
+  theme: {
+    extend: {gridTemplateRows: {
+      // Simple 8 row grid
+      '4': 'repeat(4, minmax(0, 1fr))',
+    },
+  },
+  variants: {},
+  plugins: [],
+};
+}
